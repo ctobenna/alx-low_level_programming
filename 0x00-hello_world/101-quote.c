2f230192092c8aee928cdbc185cd4bf199ef65b7
Tobenna Calistus Chukwu <ctobenna@gmail.com>
2023-06-16 15:17:32 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - print that piece of art is useful -dora korpar, 2015-10-19
- * return 1 success
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ *   followed by a new line, to the standard error.
+ *   Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
