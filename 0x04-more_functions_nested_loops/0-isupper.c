65f8a48ae3a38b3ef17d835430644e4c9c290f73
Tobenna Calistus Chukwu <ctobenna@gmail.com>
2023-06-22 13:53:34 -0700

    uncompleted task

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - check for lower character
+ * @c: character to be checked
+ * Return: 1 for uppercase or 0 for anything else
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 || c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
