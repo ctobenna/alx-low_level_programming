e5e4d66cd8f791c83620c9cabe51b91e13f21cc2
Tobenna Calistus Chukwu <ctobenna@gmail.com>
2023-06-23 11:33:14 -0700

    3-print_numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * print_numbers - a function that print 0 to 9
- * no user input
- * Return: 0 for success or 1 when fail
- */
+ * print_numbers - prints numbers from 0 to 9
+ * Return: 0
+*/
 
 void print_numbers(void)
 {
@@ -12,7 +11,7 @@ void print_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		_putchar(i + '0' );
+		_putchar(i + '0');
 	}
 	_putchar('\n');
 }
