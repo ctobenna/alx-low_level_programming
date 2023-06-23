199db6dee62ed0f27b64dfa2e359a9d4cc299772
Tobenna Calistus Chukwu <ctobenna@gmail.com>
2023-06-23 11:09:10 -0700

    2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplication of two variable 
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: a * b 
+ */
+
+int mul(int a, int b)
+{
+	int m;
+
+	m = a * b;
+	return (m);
+}
